c6770976e1baeddcb6e427edf9db160d27cc5feb
rukatt <sanctionsmith@gmail.com>
2015-09-09 13:29:24 +0200

    Update cli.c

--- cli.c
+++ cli.c
@@ -6,8 +6,8 @@
 
 void print_help() 
 {
-    printf("Usage : fft2osc [-options] sourceFile [destinationFile] \n \
-    -h : Print this message \n \
+	printf("Usage : fft2osc [-options] sourceFile [destinationFile] \n \
+	-h : Print this message \n \
 	-s : Defines the FFT size (has to be a power of 2 between 16 and 65536, default is '512') \n \
 	-t : Defines the time-stretching factor ('2' makes the file 2 times longer, '0.5' 2 times shorter, default is '1') \n \
 	-p : Defines the pitch-shifting factor ('2' rises the pitch an octave above, '0.5 an octave below, default is '1') \n \
@@ -17,33 +17,33 @@ void print_help()
 Params get_params(int argc, char** argv)
 {
 	Params params;
-    int opt;
-    int s;
-    
-    // Default parameter values
-    params.correct_params = true;
-    params.fft_size = 512;
+	int opt;
+	int s;
+	
+	// Default parameter values
+	params.correct_params = true;
+	params.fft_size = 512;
 	params.pitch_shift_factor = 1;
 	params.time_stretch_factor = 1;
     
     while ((opt = getopt(argc, argv, "s:t:p:h")) != -1) {
         switch (opt) {
-			case 's' :
+		case 's' :
 			s = atoi(optarg);
 			if ((s & (s-1)) != 0 || s < 16 || s > 65536) {
 				params.correct_params = false;
 			}
-            params.fft_size = atoi(optarg);
-            break;
-            
-        case 't' :
-            if (atof(optarg) <= 0) {
+			params.fft_size = atoi(optarg);
+			break;
+			
+		case 't' :
+			if (atof(optarg) <= 0) {
                 params.correct_params = false;
-            }
-            params.time_stretch_factor = atof(optarg);
-            break;
-            
-        case 'p' :
+			}
+			params.time_stretch_factor = atof(optarg);
+			break;
+        
+		case 'p' :
             if (atof(optarg) <= 0) {
                 params.correct_params = false;
             }
